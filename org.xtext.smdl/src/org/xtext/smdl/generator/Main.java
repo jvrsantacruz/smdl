513e7ce780ad38e1468cd2c0c256e388e3bb4bcb
Javier Santacruz <javier.santacruz.lc@gmail.com>
2016-09-09 13:49:02 +0200

    Changes output message validation when no file is given

--- org.xtext.smdl/src/org/xtext/smdl/generator/Main.java
+++ org.xtext.smdl/src/org/xtext/smdl/generator/Main.java
@@ -23,7 +23,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		if (args.length == 0) {
-			System.err.println("Aborting: no path to EMF resource provided!");
+			System.err.println("Aborting: no path provided!");
 			return;
 		}
 		Injector injector = new SmdlStandaloneSetup().createInjectorAndDoEMFRegistration();
